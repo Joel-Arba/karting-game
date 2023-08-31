fce00baf27bbc9c432ec84d43d8096982dad728b
Joel-ARBA <joel@arbadev.com>
2023-08-31 17:32:16 +0530

    Update 1.3: Music Update Added Sound FX to the vehicle Added background music

---
+++ Assets/custom-scripts/CarSounds.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSounds : MonoBehaviour
+{
+    public float minSpeed;
+    public float maxSpeed;
+    private float currentSpeed;
+
+    private Rigidbody carRb;
+    private AudioSource carAudio;
+
+    public float minPitch;
+    public float maxPitch;
+    private float pitchFromCar;
+
+    private bool isGamePaused = false;
+
+    void Start()
+    {
+        carAudio = GetComponent<AudioSource>();
+        carRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        EngineSound();
+    }
+
+    void EngineSound()
+    {
+        if(Time.timeScale == 0)
+        {
+            carAudio.Stop();
+            return;
+        }
+
+        currentSpeed = carRb.velocity.magnitude;
+        pitchFromCar = carRb.velocity.magnitude / 5f;
+        Debug.Log("CurrentSpeed: " + currentSpeed);
+        Debug.Log("PitchFromCar: " + pitchFromCar);
+        if(currentSpeed < minSpeed)
+        {
+            carAudio.pitch = minPitch;
+        }
+
+        if(currentSpeed > minSpeed && currentSpeed < maxSpeed)
+        {
+            carAudio.pitch = minPitch + pitchFromCar;
+        }
+
+        if(currentSpeed > maxSpeed)
+        {
+            carAudio.pitch = maxPitch;
+        }
+    }
+}
